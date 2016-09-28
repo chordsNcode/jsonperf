421aeddfd896b44a722b1e467aba7d4be7c80ac9
Matt Dias <matt.dias6@gmail.com>
2016-09-27 22:55:13 -0400

    Swifty test written

---
+++ JsonPerfTests/SwiftyPerfTests.swift
@@ -0,0 +1,30 @@
+//
+//  SwiftyPerfTests.swift
+//  JsonPerf
+//
+//  Created by Matt Dias on 9/27/16.
+//  Copyright © 2016 Zipcar. All rights reserved.
+//
+
+import XCTest
+import SwiftyJSON
+
+@testable import JsonPerf
+
+class SwiftyPerfTests: XCTestCase {
+    
+    func testSwiftyPerformance() {
+
+        self.measure {
+            let json = JSON(data: self.data!)
+
+            if let items = json["items"].array {
+                _ = items.map{ 
+                    Repo(swifty: $0)
+                }
+            }
+
+        }
+    }
+    
+}
